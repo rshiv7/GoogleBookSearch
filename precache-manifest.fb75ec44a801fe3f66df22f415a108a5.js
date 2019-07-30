8599447a90ee1933b9642eabb45cc6c8d94ceb1e
Raghav Shiv <Raghav@Raghavs-MacBook-Air.local>
2019-07-29 20:43:39 -0700

    Updates

---
+++ precache-manifest.fb75ec44a801fe3f66df22f415a108a5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0edfb94f5e2379cf13dda267f529cc86",
+    "url": "/GoogleBookSearch/index.html"
+  },
+  {
+    "revision": "50858f7bbcab5aa2ddcf",
+    "url": "/GoogleBookSearch/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/GoogleBookSearch/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "50858f7bbcab5aa2ddcf",
+    "url": "/GoogleBookSearch/static/js/main.8121b944.chunk.js"
+  },
+  {
+    "revision": "4c6654b98e3b34a6a35e",
+    "url": "/GoogleBookSearch/static/js/runtime~main.dfee3e5a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/GoogleBookSearch/static/media/logo.5d5d9eef.svg"
+  }
+]);
