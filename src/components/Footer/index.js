f92c8c33917e872e2c94f0b88f166c04df71dd8a
Raghav Shiv <Raghav@calvisitor-10-105-195-31.calvisitor.1918.berkeley.edu>
2019-07-30 12:15:09 -0700

    Added Styling to Footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,7 +4,7 @@ import "./style.css";
 const Footer = () => (
 	<footer className = "page-footer">
 		<div className = "container-fluid text-center">
-			<p>Raghav Shiv </p>
+			<p> Raghav Shiv </p>
 		</div>	
 	</footer>
 );
